ef5e068052059df2af16e5c989aafbefa792e3a0
merto4ka <merta@student-10-245-132-2.wlan.dur.ac.uk>
2017-03-31 17:32:43 +0100

    code

--- yelp.py
+++ yelp.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import rauth, time, json
 
 
@@ -53,36 +53,3 @@ def main():
      
 if __name__=="__main__":
 	main()
-=======
-def get_search_parameters(lat,long):
-  #See the Yelp API for more details
-  params = {}
-  params["term"] = "restaurant"
-  params["ll"] = "{},{}".format(str(lat),str(long))
-  params["radius_filter"] = "2000"
-  params["limit"] = "10"
- 
-  return params
-
-  def get_results(params):
- 
-  #Obtain these from Yelp's manage access page
-  consumer_key = "iSH3hBJx1Ij7TRwVnGzYCg" 
-  consumer_secret = "1h0M4HbgwNQQfu0MBj-QrT8Buho"
-  token = "JI210sOcR0HTa4QEWAZcxRnqXNU9SfKO"
-  token_secret = "oPOACj_tuslalhGhBZAHJ-182aQ"
-   
-  session = rauth.OAuth1Session(
-    consumer_key = consumer_key
-    ,consumer_secret = consumer_secret
-    ,access_token = token
-    ,access_token_secret = token_secret)
-     
-  request = session.get("http://api.yelp.com/v2/search",params=params)
-   
-  #Transforms the JSON API response into a Python dictionary
-  data = request.json()
-  session.close()
-   
-  return data
->>>>>>> origin/master
