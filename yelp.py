3ee03486a00db8c4852ef77162b104e39f7b4602
merto4ka <merta@student-10-245-132-2.wlan.dur.ac.uk>
2017-04-18 15:17:54 +0100

    new file html output

--- yelp.py
+++ yelp.py
@@ -2,6 +2,46 @@ import rauth, time, json
 from flask import Flask
 from flask import render_template
 from flask import request
+app = Flask("MyApp")
+
+@app.route("/")
+def front_page():
+	return render_template("yelp.html")
+
+
+@app.route("/search", methods=['POST'])
+def sign_up():
+	form_data = request.form
+	print form_data["cuisine"]
+	locations = form_data['postcode']
+	category = form_data['cuisine']
+	api_calls = []
+	#for lat,long in locations:
+	params = get_search_parameters(locations, category)
+	api_calls.append(get_results(params))
+	print len(api_calls)
+	#Be a good internet citizen and rate-limit yourself
+	time.sleep(1.0)
+	print 'error' in api_calls
+	if 'error' in api_calls:
+		print "No address"
+	else:
+		#print api_calls
+		numFound = int(api_calls[0]['total'])
+		print numFound
+		for i in range(0,numFound):
+			name = api_calls[0]['businesses'][i]['name']
+			print "Name %s." % name
+			phone = api_calls[0]['businesses'][i]['display_phone']
+			print "Phone number: %s" % phone
+			address = api_calls[0]['businesses'][i]['location']['display_address']
+			for g in range(0,len(address)):
+				print "Address: %s" % address[g]
+			rating = api_calls[0]['businesses'][i]['rating_img_url']
+			print rating
+
+	return "All OK"
+
 
 def get_search_parameters(postcode, category):
 	#See the Yelp API for more details
@@ -14,13 +54,13 @@ def get_search_parameters(postcode, category):
 	params["limit"] = "5"
 
 	return params
-def make_api_calll(offset):
-	return request.json()
+#def make_api_calll(offset):
+	#return request.json()
 
-def load_all_data():
-	data = []
-	while make_api_calll(offset)['name']:
-		name = "Name" + name
+#def load_all_data():
+	#data = []
+	#while make_api_calll(offset)['name']:
+		#name = "Name" + name
 
 
 def get_results(params):
@@ -44,9 +84,8 @@ def get_results(params):
 	
 
 	return data
-app = Flask("MyApp")
 
-@app.route("/")
+
 def main():
 	locations = raw_input()
 	category = raw_input()
@@ -72,8 +111,7 @@ def main():
 			for g in range(0,len(address)):
 				print "Address: %s" % address[g]
 			rating = api_calls[0]['businesses'][i]['rating_img_url']
-			return render_template("yelp.html")
-app.run()
+
 	#rating = api_calls[0]['businesses'][0]['rating_img_url']
 	#print "Name " + name
 	#print "Rating: " + 'rating_img_url'
@@ -82,4 +120,4 @@ app.run()
 
      
 if __name__=="__main__":
-	main()
+	app.run()
